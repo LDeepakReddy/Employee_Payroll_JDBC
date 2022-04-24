1fada4eb5261cc67b3ac137d6a8d1f93a91be70f
LDeepakReddy <l.deepakreddy.com>
2022-04-24 18:14:06 +0530

    ADDED:Function to get connected to database

--- src/main/java/EmployeePayRoll.java
+++ src/main/java/EmployeePayRoll.java
@@ -1,6 +1,10 @@
 public class EmployeePayRoll {
     public static void main(String[] args) {
         System.out.println("WELCOME TO EMPLOYEE PAYROLL JDBC..");
+
+        EmployeeConfig eConfig = new EmployeeConfig();
+        eConfig.getConfig();
     }
 }
+
 

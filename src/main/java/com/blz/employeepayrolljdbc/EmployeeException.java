d2c69fef0e0cfe17b2c9dc35127c3cd25f15a862
LDeepakReddy <l.deepakreddy.com>
2022-04-24 18:30:00 +0530

    ADDED:function to retrieve data from database

---
+++ src/main/java/com/blz/employeepayrolljdbc/EmployeeException.java
@@ -0,0 +1,9 @@
+package com.blz.employeepayrolljdbc;
+
+public class EmployeeException extends RuntimeException {
+
+    public EmployeeException(String message) {
+        super(message);
+    }
+
+}
